56ad65c81c412caac8486f65d18ac38accd11494
NESTOR GONCALVES DE RESENDE JUNIOR <nestor.junior@gmail.com>
2022-11-21 18:15:04 -0300

    ...

--- meudebian.sh
+++ meudebian.sh
@@ -104,6 +104,7 @@ then
     
     # ---- docker
     apt remove docker docker-engine docker.io containerd runc
+    rm "/usr/share/keyrings/docker-archive-keyring.gpg"
     curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
     echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" > /etc/apt/sources.list.d/docker.list
 fi
