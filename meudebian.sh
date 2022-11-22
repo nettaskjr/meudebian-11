baa73adfeb34e096da70fab730fd8283127cdca2
NESTOR GONCALVES DE RESENDE JUNIOR <nestor.junior@gmail.com>
2022-11-22 21:40:37 +0000

    ...

--- meudebian.sh
+++ meudebian.sh
@@ -103,7 +103,9 @@ then
     apt-key adv --keyserver keyserver.ubuntu.com --recv-keys ACCAF35C
     
     # ---- docker
+    # colocar o envio do erro para /dev/null
     apt remove docker docker-engine docker.io containerd runc
+    # testar se existe o arquivo
     rm "/usr/share/keyrings/docker-archive-keyring.gpg"
     curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
     echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" > /etc/apt/sources.list.d/docker.list
